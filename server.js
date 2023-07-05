9e0c31b80cbd65cfaea54b63629469064e927c6e
rohit <rohitsharma4102002@gmail.com>
2023-07-05 14:41:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// const server = require('http').createServer();
+// const io = require('socket.io')(server);
+import http from 'http';
+import {Server} from 'socket.io'
+
+const server =http.createServer()
+
+const io=new Server(server)
+
+const PORT = 3000;
+
+server.listen(PORT,()=>{
+  console.log(`Listening on port ${PORT}...`);
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
